ae217e57bc85f714f81fd62720335f1ff782b48f
Kishan Pokal <140228547+kishanpokal@users.noreply.github.com>
2025-02-04 10:49:26 +0530

    Delete css/css directory

--- css/css/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-// syntax
-// mysqli_connect("server" , "username" , "password" , "database");
-$conn = mysqli_connect("localhost" , "root" , "kishan12" , "users") or die("Connection Failed");
-?>
